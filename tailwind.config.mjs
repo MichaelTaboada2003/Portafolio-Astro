6fdc271f9438af070276da0499c01c207e87ee29
MichaelTaboada2003 <mtaboada@utb.edu.co>
2024-05-17 11:13:05 -0500

    Feat: New functionality

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class', 
+	darkMode: 'class',
 	theme: {
-		extend: {},
+	  extend: {
+		keyframes: {
+		  slideInRight: {
+			from: {
+			  opacity: '0',
+			  transform: 'translateX(-50px)',
+			},
+			to: {
+			  opacity: '1',
+			  transform: 'translateX(0)',
+			},
+		  },
+		},
+		animation: {
+		  slideInRight: 'slideInRight 1.5s ease-out forwards',
+		},
+	  },
 	},
 	plugins: [],
-}
+  }
+  
